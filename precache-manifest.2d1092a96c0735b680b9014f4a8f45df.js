3a1099d752c6f43e3df8a60568cfa9cac74366a8
Priscila Andreani <priscilaandreani@gmail.com>
2020-03-06 21:06:35 -0300

    add build

---
+++ precache-manifest.2d1092a96c0735b680b9014f4a8f45df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afed43c1f0e15a64da2906d0d4d49f6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bc5d6876befce4f94d",
+    "url": "/static/css/main.98d71655.chunk.css"
+  },
+  {
+    "revision": "9db087a3aba8eb98fe20",
+    "url": "/static/js/2.5d36276a.chunk.js"
+  },
+  {
+    "revision": "779022913f67bd8202aa70fa2261f4c5",
+    "url": "/static/js/2.5d36276a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bc5d6876befce4f94d",
+    "url": "/static/js/main.7593a087.chunk.js"
+  },
+  {
+    "revision": "26d6cdfac528e32ab0de",
+    "url": "/static/js/runtime-main.002bcbdb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
